4e713508df8b370ceb4afb1aa9f4c0972400dbca
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2019-05-21 17:59:56 +0300

    created tree flow

--- Composite/Composite/ProductListTableViewController.swift
+++ Composite/Composite/ProductListTableViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ProductListTableViewController: UITableViewController {
 
-    private var productArray = [NovaPoshta]()
-    
+    @IBOutlet fileprivate weak var backButton: UIBarButtonItem!
+    fileprivate var productArray = [NovaPoshta]()
+    fileprivate var folderFlow = [NovaPoshta]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,6 @@ class ProductListTableViewController: UITableViewController {
         foolder.addComponent(new: Product(name: "new book", price: 8))
         
         myFoolder.addComponent(new: foolder)
-
         myFoolder.addComponent(new: Product(name: "Pen", price: 40))
         myFoolder.addComponent(new: Product(name: "Pencil", price: 20))
         myFoolder.addComponent(new: Folder(name: "Middle"))
@@ -37,25 +37,40 @@ class ProductListTableViewController: UITableViewController {
         
         productArray.append(myFoolder)
         print(myFoolder.price)
-        
         tableView.reloadData()
     }
     
     @IBAction func createFolder(_ sender: Any) {
-        productArray.append(Folder(name: "New Folder"))
+        let newFolder = Folder(name: "New Folder")
+        productArray.append(newFolder)
+        folderFlow.last?.addComponent(new: newFolder)
         tableView.reloadData()
     }
     
     @IBAction func createProduct(_ sender: Any) {
-        productArray.append(Product(name: "New Product", price: 10))
+        let newProduct = Product(name: "New Product", price: 10)
+        productArray.append(newProduct)
+        folderFlow.last?.addComponent(new: newProduct)
         tableView.reloadData()
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        if folderFlow.count >= 2 {
+            folderFlow.removeLast()
+            productArray = folderFlow.last!.showContent()!
+            tableView.reloadData()
+        } else if folderFlow.count == 1 {
+            productArray = folderFlow
+            folderFlow.removeLast()
+            backButton.isEnabled = false
+            tableView.reloadData()
+        }
     }
     
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return productArray.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath) as! ProductCell
@@ -65,11 +80,14 @@ class ProductListTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if productArray[indexPath.row].contentCount() ?? -1 >= 0 {
+            tableView.deselectRow(at: indexPath, animated: false)
+            folderFlow.append(productArray[indexPath.row])
             productArray = productArray[indexPath.row].showContent()!
-            print("select boxes")
+            backButton.isEnabled = true
+            print(folderFlow)
             tableView.reloadData()
         } else {
-            print("no items or this is product")
+            tableView.deselectRow(at: indexPath, animated: true)
         }
     }
 }
